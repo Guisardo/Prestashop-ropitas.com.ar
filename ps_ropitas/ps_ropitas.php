20ed8a9ed962d420af5a976825bba3d3f850bb57
Lucas Rancez <lucas.rancez@fox.com>
2018-09-08 20:41:41 -0300

    added zones override

--- ps_ropitas/ps_ropitas.php
+++ ps_ropitas/ps_ropitas.php
@@ -4,7 +4,7 @@ class Ps_Ropitas extends Module
 	public function __construct()
 	{
 		$this->name = 'ps_ropitas';
-		$this->version = '1.1';
+		$this->version = '1.2.0';
 		$this->author = 'Guisardo';
 		$this->need_instance = 0;
 
@@ -30,6 +30,10 @@ class Ps_Ropitas extends Module
 			!Tools::copy(_PS_MODULE_DIR_.'/ps_ropitas/views/img/favicons/favicon.ico', _PS_ROOT_DIR_.'/favicon.ico')
 			||
 	        !$this->registerHook('header')
+			||
+	        !$this->registerHook('actionGetIDZoneByAddressID')
+            ||
+            !$this->createTables()
 			) {
 			return false;
 		}
@@ -37,6 +41,23 @@ class Ps_Ropitas extends Module
 		return true;
 	}
 
+	private function createTables()
+    {
+        $sql = "CREATE TABLE IF NOT EXISTS "._DB_PREFIX_."cpa_cp_1974_shipping_zone (
+	cod_postal INT NOT NULL,
+	id_zone INT UNSIGNED NOT NULL,
+	CONSTRAINT "._DB_PREFIX_."cpa_cp_1974_shipping_zone_"._DB_PREFIX_."zone_FK FOREIGN KEY (id_zone) REFERENCES "._DB_PREFIX_."zone(id_zone) ON DELETE CASCADE
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=latin1
+COLLATE=latin1_general_ci;
+";
+        if (! Db::getInstance()->Execute($sql)) {
+            return false;
+        }
+        
+        return true;
+	}
 
     public function hookHeader()
     {
@@ -46,5 +67,22 @@ class Ps_Ropitas extends Module
         $this->context->controller->addJS($this->_path.'views/js/loading.js');
 
         return $this->display(__FILE__, 'views/templates/front/header.tpl');
+    }
+
+    public function hookActionGetIDZoneByAddressID($params) {
+    	$result = false;
+        $address = new Address($params["id_address"]);
+        preg_match("/\d+/", $address->postcode, $postal_number);
+        if ($postal_number) {
+        	$cod_postal = $postal_number[0];
+        	$sql = "SELECT id_zone FROM "._DB_PREFIX_."cpa_cp_1974_shipping_zone
+WHERE cod_postal = ".$cod_postal."
+";
+	        $alt_zone = (int) Db::getInstance()->getValue($sql);
+	        if ($alt_zone) {
+	        	$result = (int) $alt_zone;
+	        }
+        }
+        return $result;
     }
 }
